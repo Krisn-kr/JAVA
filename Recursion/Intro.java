262d4c6464bd670c70d0d7dff2376a93b3345193
Krisn-18 <krishna0018kp@gmail.com>
2026-06-16 16:49:42 +0530

    sum of n Natural number

--- Recursion/Intro.java
+++ Recursion/Intro.java
@@ -4,6 +4,9 @@ public class Intro {
 
     public static void printNumb(int n){
     //  Stopping condition
+
+
+    // if not used  ----------> leads to stck overflow 
         if(n == 0){
             return;
         }
@@ -12,9 +15,25 @@ public class Intro {
         System.out.println(n);
         printNumb(n-1);
     }
+
+
+    //  second code
+
+    public static void printNumb1(int n){
+        if(n>10){
+            return;
+            
+        }
+        System.out.println(n);
+        printNumb1(n+1);
+    }
+
     public static void main(String[] args) {
         int n = 5;
+        System.out.println("Dec.......");
         printNumb(n);
+        System.out.println("Inc...");
+        printNumb1(n);
 
         
     }
---
+++ Recursion/Sum.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class Sum {
+
+    public static void printSum(int i, int n , int sum){
+        if(i==n){
+            sum +=i;
+            System.out.println(sum);
+            
+            return;
+        }
+
+        sum+=i;
+
+        printSum(i+1,n,sum);
+
+
+
+        // System.out.println(sum);
+        // printSum(n);
+    }
+   public static void main(String[] arg){
+    printSum(1,5,0);
+
+   }    
+}
