69b8116221c5cd255ceb4c0953f9b7b17eb30bca
Krisn-18 <krishna0018kp@gmail.com>
2026-08-05 00:01:16 +0530

    New changes

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -3,6 +3,43 @@ package Recursion;
 import java.util.Scanner;
 
 public class Fibonacci {
+
+    // Calculate power 
+    //  height of stack is n 
+
+    public static int calPower(int x, int n){
+        if(n ==0 ){
+            return 1;
+        }
+        if(x == 0){
+            return 0;
+        }
+        int xPownm1 = calPower(x, n-1);
+        int xPown = x * xPownm1;
+        return xPown;
+    }
+    
+    // Stack height is log n
+    public static int calculatingPower(int x , int n){
+        // Base
+        if(n == 0){
+            return 1;
+        }
+        if(x == 0){
+            return 0;
+        }
+        if(n %2 == 0){// even power ke liye
+            return calculatingPower(x, n/2) *calculatingPower(x, n/2);
+
+        }
+        else{ // odd power
+            return calculatingPower(x, n/2) * calculatingPower(x, n/2) * x;
+
+        }
+
+    }
+
+    
     public static void Fibo(int a , int b , int n){
         if(n == 0){
         return;
@@ -20,6 +57,20 @@ public class Fibonacci {
 
         System.out.print(a + " "+b);
         Fibo(a, b, n-2); // passing n-2 becaouse the previous two terms are already printed
+
+
+        //  Calling the power call
+        System.out.print("\nEnter the base and the power for calculating the base ^ power : ");
+        System.out.println("\nBase  : ");
+        int base = sc.nextInt();
+        System.out.println("\nPower : ");
+        int pow = sc.nextInt();
+
+        // Calling the method
+        System.out.println(calPower(base, pow));
+
+        // Spolution in (( n log n ))
+        System.out.println(calculatingPower(pow, n));
         sc.close();
         
     }
